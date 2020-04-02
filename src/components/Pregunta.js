5c98458b945a8e7ae853983fcdbf7d9bdfe87e75
Jose Bustillo <jabustillo@uninorte.edu.co>
2020-04-02 12:23:50 -0500

    Validation

--- src/components/Pregunta.js
+++ src/components/Pregunta.js
@@ -1,15 +1,34 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 
 const Pregunta = () => {
+    const [cantidad, setCantidad] = useState(0);
+    const [error, setError] = useState(false);
+
+    // Definir presupuesto
+
+    const agregarPresupuesto = e => {
+        e.preventDefault();
+        
+        // Validación
+        if (cantidad < 1 || isNaN(cantidad)) {
+            setError(true);
+            return;
+        }
+
+        setError(false);
+
+    };
+
     return ( 
         <Fragment>
             <h2>Coloca tu presupuesto</h2>
 
-            <form>
+            <form onSubmit={agregarPresupuesto}>
                 <input 
                     type="number"
                     className="u-full-width"
                     placeholder="Coloca tu presupuesto"
+                    onChange={e => setCantidad(parseInt(e.target.value))}
                 />
                 <input 
                     type="submit"
